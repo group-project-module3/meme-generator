d419e3594cec1f196886ea1faa03b246e88e1ca7
Abdoul Traore <atraore2@stu.ctcd.edu>
2021-11-24 19:52:07 -0600

    fix the refresh button

--- src/App.js
+++ src/App.js
@@ -7,28 +7,44 @@ class App extends Component {
     constructor() {
         super() //used to call the constructor of its parent class - required to access variables of parent class
         this.state = {
-            dataArr: [],
             inputs:[],
+            dataArr: [],
             topInput:"",
-            bottomInput:""
+            bottomInput:"",
+            currentMeme: {}
         }
+        
+        this.handleClick = this.handleClick.bind(this)
         this.inputChange = this.inputChange.bind(this)
         this.input = this.input.bind(this)
-        // this.preventReload = this.preventReload.bind(this)
     }
 
     componentDidMount() {
         axios.get("https://api.imgflip.com/get_memes")
             .then(res => {
-                // console.log(res) 
-                this.setState({
-                    dataArr: res.data.data.memes
+                // console.log(res.data.data.memes) 
+                this.setState((prevState)=> {
+                    return { 
+                        dataArr: res.data.data.memes, 
+                        random: Math.floor(Math.random() * res.data.data.memes.length),
+                        currentMeme: res.data.data.memes[Math.floor(Math.random() * res.data.data.memes.length)]
+                    }
                 })
             })
     }
+handleClick(){
+    this.setState(prevState =>{
+        let random = Math.floor(Math.random() * prevState.dataArr.length)
+        return{
+            
+            currentMeme: prevState.dataArr[random]
+        }
+    })
+
+}
 
 inputChange(event){
-    event.preventDefault()
+    // event.preventDefault()
     const { name, value } = event.target
     this.setState({
         [ name ] : value
@@ -41,25 +57,20 @@ inputs:[...prevState.inputs, `${prevState.topInput} ${prevState.bottomInput}`]
 }))
 }
 
-    render() {
-        const random = Math.floor(Math.random() * this.state.dataArr.length)
-        const select = this.state.dataArr.map(image => image.url)
-        const all = select[random]
-        // console.log(select)
-        // console.log(all)
+render() {
         return (
             <div>
               
                 <Meme 
-                random={all} 
+                random={this.state.currentMeme.url} 
                 topName={"topInput"}
                 bottomName={"bottomInput"}
                 topValue={this.state.topInput} 
                 bottomValue={this.state.bottomInput} 
                 change={this.inputChange}
                 input={this.input}
-                prevent={this.preventReload}
                 />
+                <button onClick={this.handleClick}>Refresh</button>
              
             </div>
         )
